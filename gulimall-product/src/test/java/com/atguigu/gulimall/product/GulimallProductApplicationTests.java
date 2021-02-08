5f44c3f19dd505d64b02f2dc049542c2f48bb8d2
Cai Peishen <peishen.cai@foxmail.com>
2021-02-08 18:03:52 +0800

    新建第三方模块：OSS

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -8,9 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
@@ -35,43 +32,6 @@ class GulimallProductApplicationTests {
             System.out.println(brand.getName());
         });
     
-    }
-    
-//    @Test
-//    void testOSS() throws FileNotFoundException {
-//        // Endpoint以杭州为例，其它Region请按实际情况填写。
-//        String endpoint = "https://oss-cn-shanghai.aliyuncs.com";
-//        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-//        String accessKeyId = "";
-//        String accessKeySecret = "";
-//
-//        // 创建OSSClient实例。
-//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-//
-//        // 获取文件流
-//        InputStream inputStream = new FileInputStream("C:\\Users\\peish\\Pictures\\Camera Roll\\头像3.JPG");
-//
-//        // bucket、fileName、fileStream
-//        ossClient.putObject("gulimall-ferris", "头像.jpg", inputStream);
-//
-//        // 关闭OSSClient。
-//        ossClient.shutdown();
-//
-//        System.out.println("上传成功...");
-//    }
-    
-    @Test
-    void testSpringCloudAlibabaOSS() throws FileNotFoundException {
-        // 获取文件流
-        InputStream inputStream = new FileInputStream("C:\\Users\\peish\\Pictures\\Camera Roll\\头像3.JPG");
-
-        // bucket、fileName、fileStream
-        ossClient.putObject("gulimall-ferris", "头像2.jpg", inputStream);
-
-        // 关闭OSSClient。
-        ossClient.shutdown();
-
-        System.out.println("上传成功...");
     }
     
 }
