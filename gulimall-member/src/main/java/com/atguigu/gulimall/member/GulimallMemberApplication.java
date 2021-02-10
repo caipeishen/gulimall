94c5e9ee7c4236f3d76732feb1c224d63ef1007a
Cai Peishen <peishen.cai@foxmail.com>
2021-02-10 17:14:14 +0800

    配置会员服务gateway

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -2,6 +2,7 @@ package com.atguigu.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * 3.编写feign远程调用类(1.调用的服务 2.调用的具体接口)
  */
 @EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
     
