ff7eefbc4e754176f736b99db0121fe94417e668
Cai Peishen <peishen.cai@foxmail.com>
2021-02-24 16:24:24 +0800

    Redisson闭锁

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.product.web;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
 import com.atguigu.gulimall.product.vo.Catelog2Vo;
+import org.redisson.api.RCountDownLatch;
 import org.redisson.api.RLock;
 import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -126,4 +128,27 @@ public class IndexController {
         }
         return s;
     }
+    
+    /**
+     * 闭锁 只有设定的人全通过才关门
+     */
+    @ResponseBody
+    @GetMapping("/index/lockDoor")
+    public String lockDoor() throws InterruptedException {
+        RCountDownLatch door = redissonClient.getCountDownLatch("door");
+        // 设置这里有5个人
+        door.trySetCount(5);
+        door.await();
+        return "5个人全部通过了...";
+    }
+    
+    @ResponseBody
+    @GetMapping("/index/go/{id}")
+    public String go(@PathVariable("id") Long id) throws InterruptedException {
+        RCountDownLatch door = redissonClient.getCountDownLatch("door");
+        // 每访问一次相当于出去一个人
+        door.countDown();
+        return id + "走了";
+    }
+    
 }
