3332e03e1abc89d3ed0c238f2be9e8db8285ba55
Cai Peishen <peishen.cai@foxmail.com>
2021-03-13 18:57:38 +0800

    spring-session整合解决session共享

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+// 开启redis 存储session
+@EnableRedisHttpSession
 @EnableFeignClients("com.atguigu.gulimall.auth.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * 1、整合MyBatis-Plus
@@ -90,6 +91,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *              原理:
  *                  CacheManager(RediscacheManager)->cache(RedisCache)->cache负责缓存的读写
  **/
+// 开启redis 存储session
+@EnableRedisHttpSession
 @EnableFeignClients(basePackages = "com.atguigu.gulimall.product.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
