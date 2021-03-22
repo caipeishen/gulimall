008a0ba63f8413891b9fbdb91dba1749c744c6e2
Cai Peishen <peishen.cai@foxmail.com>
2021-03-22 16:19:14 +0800

    订单服务添加rabbit消息队列

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,21 @@
 package com.atguigu.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+/**
+ * 使用RabbitMQ
+ * 1、引Amqp场景;RabbitAutoConfiguration就会自动生效
+ *      所有的属性都是 spring.rabbitmq
+ *      ConfigurationProperties(prefix = "spring.rabbitmq")
+ *      public class RabbitProperties{ }
+ * 2、给容器中自动配置了 RabbitTemplate、AmqpAdmin、CachingConnectionFactory、RabbitMessagingTemplate
+ * 3、@EnableRabbit: @EnableXXX
+ */
+@EnableRabbit
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallOrderApplication {
--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,11 +1,19 @@
 package com.atguigu.gulimall.order;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallOrderApplicationTests {
-    
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    /**
+     *  1. 使用AmqpAdmin创建Exchange、Queue、Binding
+     */
     @Test
     void contextLoads() {
     }
