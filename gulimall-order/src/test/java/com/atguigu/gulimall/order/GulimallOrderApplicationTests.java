d88dd2cb9e98befe65e570aeabe7fc63a29d9531
Cai Peishen <peishen.cai@foxmail.com>
2021-03-22 16:55:39 +0800

    使用AmqpAdmin创建Exchange、Queue、Binding

--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,10 +1,15 @@
 package com.atguigu.gulimall.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class GulimallOrderApplicationTests {
 
@@ -14,8 +19,33 @@ class GulimallOrderApplicationTests {
     /**
      *  1. 使用AmqpAdmin创建Exchange、Queue、Binding
      */
+
+
     @Test
-    void contextLoads() {
+    void createExchange() {
+        // String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
+        DirectExchange exchange = new DirectExchange("hello-java-exchange", true, false);
+        this.amqpAdmin.declareExchange(exchange);
+        log.info("Exchange [" + exchange.getName() + "] 创建成功");
     }
-    
+
+    @Test
+    void createQueue() {
+        // String name, boolean durable, boolean exclusive, boolean autoDelete, @Nullable Map<String, Object> arguments
+        Queue queue = new Queue("hello-java-queue", true, false, false);
+        this.amqpAdmin.declareQueue(queue);
+        log.info("Queue [" + queue.getName() + "] 创建成功");
+    }
+
+    @Test
+    void createBinding() {
+        // String destination[目的地], Binding.DestinationType destinationType[目的地类型], String exchange[交换机名称], String routingKey[路由键], @Nullable Map<String, Object> arguments[参数]
+        Binding binding = new Binding("hello-java-queue",
+                Binding.DestinationType.QUEUE,
+                "hello-java-exchange",
+                "hello.java",
+                null);
+        this.amqpAdmin.declareBinding(binding);
+        log.info("Binding [" + "hello-java-binding" + "] 创建成功");
+    }
 }
