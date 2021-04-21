6c159e1e367bdcaf8200392a4c21e748d57e766f
Cai Peishen <peishen.cai@foxmail.com>
2021-04-21 10:19:37 +0800

    @SentinelResource注解方式限流熔断降级

--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/service/impl/SeckillServiceImpl.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/service/impl/SeckillServiceImpl.java
@@ -1,5 +1,7 @@
 package com.atguigu.gulimall.seckill.service.impl;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.atguigu.common.to.mq.SeckillOrderTo;
@@ -34,7 +36,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Slf4j
-@Service("secKillService")
+@Service("seckillService")
 public class SeckillServiceImpl implements SeckillService {
     
     @Autowired
@@ -72,8 +74,14 @@ public class SeckillServiceImpl implements SeckillService {
             this.saveSessionSkuInfo(sessions);
         }
     }
-
+    
+    /**
+     * blockHandler：处理限流熔断降级
+     * fallback：处理所有异常
+     * @return
+     */
     @Override
+    @SentinelResource(value = "seckillSkusResource", blockHandler = "seckillSkusBlockHandler", fallback = "seckillSkusFallBack")
     public List<SeckillSkuRedisTo> getCurrentSeckillSkus() {
         // 1.确定当前时间属于那个秒杀场次
         long time = new Date().getTime();
@@ -101,6 +109,33 @@ public class SeckillServiceImpl implements SeckillService {
         }
         return null;
     }
+    
+    /**
+     * blockHandler / blockHandlerClass: blockHandler 对应处理 BlockException 的函数名称，可选项。
+     * blockHandler 函数访问范围需要是 public，返回类型需要与原方法相匹配，参数类型需要和原方法相匹配并且最后加一个额外的参数，类型为 BlockException。
+     * blockHandler 函数默认需要和原方法在同一个类中。若希望使用其他类的函数，则可以指定 blockHandlerClass 为对应的类的 Class 对象，注意对应的函数必需为 static 函数，否则无法解析
+     * @param blockException
+     * @return
+     */
+    public List<SeckillSkuRedisTo> seckillSkusBlockHandler(BlockException blockException) {
+        log.info("限流...seckillSkusBlockHandler....");
+        return null;
+    }
+    
+    /**
+     * fallback / fallbackClass：fallback 函数名称，可选项，用于在抛出异常的时候提供 fallback 处理逻辑。
+     * fallback 函数可以针对所有类型的异常（除了 exceptionsToIgnore 里面排除掉的异常类型）进行处理。fallback 函数签名和位置要求：
+     *      返回值类型必须与原函数返回值类型一致；
+     *      方法参数列表需要和原函数一致，或者可以额外多一个 Throwable 类型的参数用于接收对应的异常。
+     *      fallback 函数默认需要和原方法在同一个类中。若希望使用其他类的函数，则可以指定 fallbackClass 为对应的类的 Class 对象，注意对应的函数必需为 static 函数，否则无法解析。
+     * @param throwable
+     * @return
+     */
+    public List<SeckillSkuRedisTo> seckillSkusFallBack(Throwable throwable) {
+        log.info("异常...seckillSkusFallBack...");
+        return null;
+    }
+    
 
     @Override
     public SeckillSkuRedisTo getSkuSeckillInfo(Long skuId) {
